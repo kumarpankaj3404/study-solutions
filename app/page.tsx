dc85003adb96548b12fed3f29e190c4f743fbd4d
Pankaj Kumar <142082497+kumarpankaj3404@users.noreply.github.com>
2026-02-12 16:45:56 +0530

    Setup initial project

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      Page
+      <Button>Let's start</Button>
+    </div>
+  )
+}
+
+export default Page
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import page from '../page'
+
+const Page = () => {
+  return (
+    <div>
+        Sign In Page
+    </div>
+  )
+}
+
+export default Page
