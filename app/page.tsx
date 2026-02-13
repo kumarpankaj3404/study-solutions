7a9d8614c54713fb0973c79d489b53f6cb989097
Pankaj Kumar <142082497+kumarpankaj3404@users.noreply.github.com>
2026-02-13 22:18:18 +0530

    Implement Companion Card and List with CTA section.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,47 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
+import { recentSessions } from '@/constants/index'
 
 const Page = () => {
   return (
-    <div>
-      Page
-      <Button>Let's start</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl ">Popular Companions</h1>
+        <section className="home-section">
+            <CompanionCard 
+              id="123"
+              name="Neural the Binary Explorer"
+              topic="Neural Network in Brain"
+              subject="Science"
+              duration={45}
+              color="#ffda6e"
+            />
+            <CompanionCard 
+              id="1234"
+              name="Countsy the Number Wizard"
+              topic="Derivatives & Integrals"
+              subject="Math"
+              duration={30}
+              color="#e5d0ff"
+            />
+            <CompanionCard 
+              id="12345"
+              name="Verba the Vocabulary Builder"
+              topic="English Literature"
+              subject="Language"
+              duration={30}
+              color="#BDE7FF"
+            />
+        </section>
+        <section className='home-section'>
+            <CompanionsList
+              title="Recently completed sessions"
+              companions={recentSessions}
+              classNames="w-2/3 max-lg:w-full"
+            />
+            <CTA/>
+        </section>
+    </main>
   )
 }
 
